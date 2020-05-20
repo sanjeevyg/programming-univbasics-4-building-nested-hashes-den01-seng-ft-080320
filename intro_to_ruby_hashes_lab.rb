cab32fcd3b043102e6741a3afe86aefda4bdb4b1
Sanjeev Yogi <yogi.sjv@gmail.com>
2020-05-20 04:34:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,7 @@ def monopoly_with_second_tier
   new_hash = {railroads: {}}
   new_hash[:railroads][:pieces] = 4
   new_hash[:railroads][:rent_in_dollars] = {}
-  new_hash[:railroads] = {}
+  new_hash[:railroads][:names] = {}
   new_hash
 end
 
