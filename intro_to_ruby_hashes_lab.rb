4098e0f7c72d0c5b45e3e1677b21c9f2cbdcb4b4
Sanjeev Yogi <yogi.sjv@gmail.com>
2020-05-20 04:38:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   new_hash = monopoly_with_second_tier
-  new
+  new_hash[:railroads][:rent_in_dollars] = {:one_piece
 end
 
 def monopoly_with_fourth_tier
