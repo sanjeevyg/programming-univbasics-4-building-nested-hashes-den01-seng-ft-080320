2642c02a06cc5aa52beede1228a63071a99f1925
Sanjeev Yogi <yogi.sjv@gmail.com>
2020-05-20 04:40:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,6 +23,7 @@ def monopoly_with_third_tier
   # complexity.
   new_hash = monopoly_with_second_tier
   new_hash[:railroads][:rent_in_dollars] = {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, :four_pieces_owned: 200}
+  new_hash[:railroads][:names] = {reading_railroad: {}, 
 end
 
 def monopoly_with_fourth_tier
